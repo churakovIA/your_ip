d242fee5717e5fed5a6761b1d66f1124dca22f73
Igor Churakov <churakovpost@gmail.com>
2020-05-05 17:45:29 +0300

    Add exception handler

---
+++ src/main/java/com/github/churakovIA/util/exeption/ErrorInfo.java
@@ -0,0 +1,32 @@
+package com.github.churakovIA.util.exeption;
+
+public class ErrorInfo {
+
+  private final String uri;
+  private final Integer statusCode;
+  private final String servletName;
+  private final String[] details;
+
+  public ErrorInfo(CharSequence uri, Integer statusCode, String servletName, String... details) {
+    this.uri = uri.toString();
+    this.statusCode = statusCode;
+    this.servletName = servletName;
+    this.details = details;
+  }
+
+  public String getUri() {
+    return uri;
+  }
+
+  public Integer getStatusCode() {
+    return statusCode;
+  }
+
+  public String getServletName() {
+    return servletName;
+  }
+
+  public String[] getDetails() {
+    return details;
+  }
+}
---
+++ src/main/java/com/github/churakovIA/web/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.github.churakovIA.web;
+
+import com.github.churakovIA.util.JsonUtil;
+import com.github.churakovIA.util.ValidationUtil;
+import com.github.churakovIA.util.exeption.ErrorInfo;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/GlobalExceptionHandler")
+public class GlobalExceptionHandler extends AbstractServlet {
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    Throwable throwable = (Throwable) req.getAttribute("javax.servlet.error.exception");
+    Integer statusCode = (Integer) req.getAttribute("javax.servlet.error.status_code");
+    String servletName = (String) req.getAttribute("javax.servlet.error.servlet_name");
+    if (servletName == null) {
+      servletName = "Unknown";
+    }
+    String requestUri = (String) req.getAttribute("javax.servlet.error.request_uri");
+    if (requestUri == null) {
+      requestUri = "Unknown";
+    }
+
+    PrintWriter out = resp.getWriter();
+    resp.setContentType("application/json");
+    resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    Throwable rootCause = ValidationUtil.getRootCause(throwable);
+    ErrorInfo errorInfo = new ErrorInfo(requestUri, statusCode, servletName, rootCause.toString());
+    JsonUtil.writeValue(out, errorInfo);
+  }
+}
