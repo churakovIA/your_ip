3a93488b55e60114dd055d2753809186a52c950f
Igor Churakov <churakovpost@gmail.com>
2020-07-27 15:23:44 +0300

    Add logging

--- src/main/java/com/github/churakovIA/web/RequestTextServlet.java
+++ src/main/java/com/github/churakovIA/web/RequestTextServlet.java
@@ -5,12 +5,17 @@ import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @WebServlet("/rq")
 public class RequestTextServlet extends AbstractServlet {
+  private final static Logger log = LoggerFactory.getLogger(RequestTextServlet.class);
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    log.debug("Request InputStream: {}",
+        req.getInputStream() == null ? "null" : req.getInputStream().available());
     resp.setContentType("text/plain");
     RequestInfoMapper.getInstance(req).toString(resp.getWriter());
   }
